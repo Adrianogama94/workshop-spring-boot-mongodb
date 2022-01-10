55bc8bfe06b031147c7ff99ebe9f325fa045d63c
Adriano Gama <adrianosg94@gmail.com>
2022-01-10 13:27:13 -0300

    Referenciando os Posts com o User

--- src/main/java/com/gama/workshopmongo/config/Instantiation.java
+++ src/main/java/com/gama/workshopmongo/config/Instantiation.java
@@ -44,6 +44,10 @@ public class Instantiation implements CommandLineRunner{
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
+		adriano.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(adriano);
+		
+		
 	}
 
 }
